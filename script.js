fe2660cd26913fbb51981bee16f492a8c28bae4f
Nicode1203 <nicolasperezb1203@gmail.com>
2024-10-22 10:37:25 -0500

    Add hover to the squares and also created button to add squares

--- script.js
+++ script.js
@@ -5,14 +5,34 @@ gridContainer.style.flexWrap = "wrap";  // Permite que los elementos se ajusten 
 gridContainer.style.width = "256px";    // Establece el ancho total del contenedor (16 * 16px)
 gridContainer.style.border = "2px solid black";
 
+const btnChoice = document.createElement("button");
+btnChoice.textContent = "how many squares(pixels) do you want?";
+
+btnChoice.addEventListener("click", () =>{
+    const numSquares = prompt("Select the number of squares you want (eg 100 = 100x100), Maximum number 100");
+    const numSquaresValue = Number (numSquares);
+
+    if (numSquaresValue < 100 || numSquaresValue > 0 || isNaN(numSquaresValue)){
+        alert("Please enter a valid number (between 1 and 100)");
+        return;
+    }
+    
+
+})
+
 for (let i = 0; i < 16 * 16; i++){
     const gridSquare = document.createElement("div");
     gridSquare.style.width = "14px";  // Ajustar el tamaño para compensar el borde
     gridSquare.style.height = "14px"; // Ajustar el tamaño para compensar el borde
     gridSquare.style.backgroundColor  = "lightgray";
     gridSquare.style.border = "1px solid gray";
+
+    gridSquare.addEventListener("mouseenter", () =>{
+        gridSquare.style.backgroundColor = "black";
+    })
     gridContainer.appendChild(gridSquare);
 }
 
 document.body.appendChild(mainBox);
+mainBox.appendChild(btnChoice);
 mainBox.appendChild(gridContainer);
